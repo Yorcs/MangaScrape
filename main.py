7ae787d33d177ed2acf1d3c2eddbabb66900c1a5
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-03-17 21:39:53 -0700

    Added If statements
    If it's published after the year 2000, then it will be displayed

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ mangas = soup.find_all('div', class_ = 'col-12 col-lg-6 p-3 text')
 
 for manga in mangas:
     mangaSubInfo = manga.find('div', class_ = 'col text p-1 pl-3')
-    genre = manga.find('div', class_ = 'textsmall').text
-    mangaName = mangaSubInfo.find('a', attrs={"alt" : "Series Info"}).text
     yearPublishedAndRating = mangaSubInfo.findAll('div', class_ = 'text')[2].text
     yearPublished = '%.4s' % yearPublishedAndRating
+    
+    if(int(yearPublished) > 2000):
+        genre = manga.find('div', class_ = 'textsmall').text
+        mangaName = mangaSubInfo.find('a', attrs={"alt" : "Series Info"}).text
 
-    print (f'''
-    mangaName = {mangaName}
-    genre = {genre}
-    yearPublished = {yearPublished}
-    ''')
-    print('')
+        print (f'''
+        mangaName = {mangaName}
+        genre = {genre}
+        yearPublished = {yearPublished}
+        ''')
+        print('')
