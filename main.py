52930786d0ba17c357f26f4041719d014d3d0f5e
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-03-17 20:47:16 -0700

    Add scrape name, year

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ html_text = requests.get('https://www.mangaupdates.com/series.html?perpage=100')
 
 soup = BeautifulSoup(html_text, 'lxml')
 manga = soup.find('div', class_ = 'col-12 col-lg-6 p-3 text')
+mangaSubInfo = manga.find('div', class_ = 'col text p-1 pl-3')
+
 genre = manga.find('div', class_ = 'textsmall').text
+mangaName = mangaSubInfo.find('a', attrs={"alt" : "Series Info"}).text
+yearPublished = mangaSubInfo.findAll('div', class_ = 'text')[2].text
 print(genre)
+print(mangaName)
+print(yearPublished)
