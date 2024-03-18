a94f2cd74bafbb8ed860f7e121b5b2e89e5004de
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-03-18 15:06:55 -0700

    Adding enumerator, generated outcome examples

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def find_manga():
     soup = BeautifulSoup(html_text, 'lxml')
     mangas = soup.find_all('div', class_ = 'col-12 col-lg-6 p-3 text')
 
-    # # Looping to scrape data and printing the result
-    for manga in mangas:
+    # # Looping to scrape data and f.writeing the result
+    for index, manga in enumerate(mangas):
         manga_subinfo = manga.find('div', class_ = 'col text p-1 pl-3')
         year_published_and_rating = manga_subinfo.findAll('div', class_ = 'text')[2].text
         year_published = '%.4s' % year_published_and_rating
@@ -35,21 +35,23 @@ def find_manga():
 
                 # Checking if the user input matches the genre scraped from the website
                 if not any(individual_exclude_genre in multiple_genre for individual_exclude_genre in genre_split_lowercase):
-                    print(f"Manga Name: {manga_name.strip()}")
-                    print(f"Genre: {genre.strip()}")
-                    print(f"Year Published: {year_published.strip()}")
-                    print(f"More Info: {more_info}")
+                    with open(f'posts/{index}.txt', 'w', encoding="utf-8") as f:
+                        f.write(f"Manga Name: {manga_name.strip()} \n")
+                        f.write(f"Genre: {genre.strip()} \n")
+                        f.write(f"Year Published: {year_published.strip()} \n")
+                        f.write(f"More Info: {more_info} \n")
+                    print(f'File saved: {index}')
 
-                    print('')
             else:
                 if exclude_genre not in genre_split_lowercase:
-                    print(f"Manga Name: {manga_name.strip()}")
-                    print(f"Genre: {genre.strip()}")
-                    print(f"Year Published: {year_published.strip()}")
-                    print(f"More Info: {more_info}")
+                    with open(f'posts/{index}.txt', 'w', encoding="utf-8") as f:
+                        f.write(f"Manga Name: {manga_name.strip()} \n")
+                        f.write(f"Genre: {genre.strip()} \n")
+                        f.write(f"Year Published: {year_published.strip()} \n")
+                        f.write(f"More Info: {more_info} \n")
+                    print(f'File saved: {index}')
 
-                    print('')
-
+# Run Code every 10 minutes
 if __name__ =='__main__':
     while True:
         find_manga()
