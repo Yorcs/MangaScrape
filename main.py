1d207b7eebcdeb1dfb783c48855d1cf12fe5b172
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-03-17 21:33:58 -0700

    added looping

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import requests
 html_text = requests.get('https://www.mangaupdates.com/series.html?perpage=100').text
 
 soup = BeautifulSoup(html_text, 'lxml')
-manga = soup.find('div', class_ = 'col-12 col-lg-6 p-3 text')
-mangaSubInfo = manga.find('div', class_ = 'col text p-1 pl-3')
+mangas = soup.find_all('div', class_ = 'col-12 col-lg-6 p-3 text')
 
-genre = manga.find('div', class_ = 'textsmall').text
-mangaName = mangaSubInfo.find('a', attrs={"alt" : "Series Info"}).text
-yearPublished = mangaSubInfo.findAll('div', class_ = 'text')[2].text
-print(genre)
-print(mangaName)
-print(yearPublished)
+for manga in mangas:
+    mangaSubInfo = manga.find('div', class_ = 'col text p-1 pl-3')
+    genre = manga.find('div', class_ = 'textsmall').text
+    mangaName = mangaSubInfo.find('a', attrs={"alt" : "Series Info"}).text
+    yearPublishedAndRating = mangaSubInfo.findAll('div', class_ = 'text')[2].text
+    yearPublished = '%.4s' % yearPublishedAndRating
+
+    print (f'''
+    mangaName = {mangaName}
+    genre = {genre}
+    yearPublished = {yearPublished}
+    ''')
+    print('')
